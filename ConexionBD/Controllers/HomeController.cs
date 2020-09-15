be80f291ed1d6eacc6aa87b362d14935ea40b618
TLEMEX <TLEMEX@LAPTOP-1PVF4VVF>
2020-09-15 17:21:11 -0500

    Pracrica 2

---
+++ ConexionBD/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ConexionBD.Models;
+
+namespace ConexionBD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            mexicoContext context = new mexicoContext();
+            var estados = context.Estados.OrderByDescending(x=> x.Nombre).ToList();
+            return View(estados);
+        }
+    }
+}
---
+++ ConexionBD/Models/Estados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConexionBD.Models
+{
+    public partial class Estados
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Abrev { get; set; }
+    }
+}
