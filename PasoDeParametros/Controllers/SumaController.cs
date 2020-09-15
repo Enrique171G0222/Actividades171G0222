78b09a3adda8bea0504a1c0c813507353477858f
TLEMEX <TLEMEX@LAPTOP-1PVF4VVF>
2020-09-14 19:53:01 -0500

    /Suma/SolicitarNumeros es el uri del trabajo

---
+++ PasoDeParametros/Controllers/SumaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PasoDeParametros.Controllers
+{
+    public class SumaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Resultado(int numero1, int numero2)
+        {
+            int res = numero1 + numero2;
+            return View(res);
+        }
+        public IActionResult SolicitarNumeros()
+        {
+            return View();
+        }
+    }
+}
---
+++ PasoDeParametros/Models/Numeros.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PasoDeParametros.Models
+{
+    public class Numeros
+    {
+        public int numero1 { get; set; }
+        public int numero2 { get; set; }
+    }
+}
